7740039eba1d9dc92944a2e8cc71ad6d2e44b834
Thales Rodrigues <thales.brito@hotmail.com>
2021-12-30 14:05:18 -0300

    Basic server class

---
+++ lib/network/Server.ts
@@ -0,0 +1,137 @@
+import { KeyValue } from "../utils/KeyValue";
+
+declare function require(name: string): any;
+const WS = require("ws");
+
+interface ServerConfig {
+  debug?: boolean;
+  port?: number;
+  //   mainRoom?: typeof Room;
+  //   entities: { [k: string]: typeof Entity };
+  wsOptions?: KeyValue;
+
+  /**
+   * How many ticks will happen per second (default is 64)
+   */
+  tickRate?: number;
+
+  // on?: {
+  //   connection?: (user: User) => void,
+  //   disconnection?: (user: User) => void,
+  //   message?: (user: User, message: KeyValue) => void,
+
+  //   /**
+  //    * This function will be called every server tick
+  //    */
+  //   tick?: (server: Server) => void|(() => void),
+  // },
+}
+
+const DEFAULT_PORT = 3000;
+const DEFAULT_TICK_RATE = 64;
+
+export class Server {
+  static current: Server;
+
+  private ws: any = undefined;
+  private tickIntervalRef: any = undefined;
+
+  public get config() {
+    return this._config;
+  }
+  private _config: ServerConfig;
+
+  /**
+   * Server's default room.
+   *
+   * All users automatically join this room when they connect to the server.
+   */
+  // public mainRoom: Room;
+
+  /**
+   * How many tick events will happen per second
+   */
+  public get tickRate() {
+    return this.config.tickRate ?? DEFAULT_TICK_RATE;
+  }
+
+  /**
+   * Counts how many ticks have happened since the server started
+   */
+  public get ticks() {
+    return this._ticks;
+  }
+  private _ticks: number = 0;
+
+  /**
+   * Measures the time (in seconds) elapsed since the last server tick
+   */
+  public get deltaTime() {
+    return (new Date().getTime() - this._lastTickTimestamp) / 1000;
+  }
+  private _lastTickTimestamp: number = 0;
+
+  /**
+   * Measures the time (in seconds) elapsed since the initialization of the server
+   */
+  public get time() {
+    return (new Date().getTime() - this._serverStartTimestamp) / 1000;
+  }
+  private _serverStartTimestamp: number = 0;
+
+  constructor(config: ServerConfig) {
+    Server.current = this;
+
+    config.port ??= DEFAULT_PORT;
+    config.tickRate ??= DEFAULT_TICK_RATE;
+
+    this._config = config;
+  }
+
+  private _users:any[] = [];
+  public get users() { return this._users };
+
+    /**
+   * Closes the server and disconnects all users
+   */
+     stop(): Server {
+      this.ws?.close();
+      clearInterval(this.tickIntervalRef);
+      return this;
+    }
+
+  /**
+   * Initializes the server
+   */
+  start(): Server {
+    const wss = new WS.Server({
+      port: this.config.port,
+      ...this.config.wsOptions,
+    });
+    this.ws = wss;
+
+    console.log(`SharedIO server running on port ${this.config.port}`);
+
+    this._serverStartTimestamp = this._lastTickTimestamp = new Date().getTime();
+
+    this.tickIntervalRef = setInterval(() => {
+      this._ticks ++;
+
+      this._lastTickTimestamp = new Date().getTime();
+    }, 1000 / this.tickRate);
+
+    wss.on("connection", (ws: any) => {
+      console.log("New user connected");
+
+      const userIndex = this._users.length;
+      this._users.push(ws);
+
+      ws.on("close", () => {
+        console.log("User disconnected");
+        this._users.splice(userIndex, 1);
+      })
+    });
+
+    return this;
+  }
+}
---
+++ test/manual/index.ts
@@ -0,0 +1,5 @@
+import { Server } from "../../";
+
+const server = new Server({
+    port: 8080,
+}).start();
