ddb13c94af7aeadf6d0cc845d2672fc234b119a3
Thales Rodrigues <thales.brito@hotmail.com>
2022-01-21 22:39:35 -0300

    Improve rules description

--- lib/schema/Decorators.ts
+++ lib/schema/Decorators.ts
@@ -9,6 +9,8 @@ function prepareSchemaAndGetType(entity: Entity) {
 }
 
 /**
+ * @SharedIO Rule Decorator
+ *
  * All users can read this attribute
  */
  export function Public(entity: Entity, attributeName: string) {
@@ -21,6 +23,8 @@ function prepareSchemaAndGetType(entity: Entity) {
 }
 
 /**
+ * @SharedIO Rule Decorator
+ *
  * Only the entity's owner can read this attribute
  */
 export function Private(entity: Entity, attributeName: string) {
@@ -33,6 +37,8 @@ export function Private(entity: Entity, attributeName: string) {
 }
 
 /**
+ * @SharedIO Rule Decorator
+ *
  * No user can read this attribute, it's server-side only
  */
 export function Internal(entity: Entity, attributeName: string) {
@@ -45,6 +51,8 @@ export function Internal(entity: Entity, attributeName: string) {
 }
 
 /**
+ * @SharedIO Rule Decorator
+ *
  * Users are not allowed to edit this attribute
  */
 export function Readonly(entity: Entity, attributeName: string) {
--- lib/schema/index.ts
+++ lib/schema/index.ts
@@ -2,5 +2,5 @@ export * from "./HasId";
 export * from "./User";
 export * from "./Entity";
 export * from "./Rules";
-export * from "./Modifiers";
+export * from "./Decorators";
 export * from "./View";
