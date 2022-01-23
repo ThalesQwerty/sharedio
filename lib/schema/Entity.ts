4ccf1172341db2c51b43db6c245709d6c7659baa
Thales Rodrigues <thales.brito@hotmail.com>
2022-01-22 21:52:20 -0300

    Reorganizing Entity class

--- lib/schema/Entity.ts
+++ lib/schema/Entity.ts
@@ -1,21 +1,12 @@
 import { User, HasId } from ".";
 import { Server } from "../connection";
-import { KeyValue } from "../types/KeyValue";
+import { KeyValue, Hooks } from "../types";
 
 import * as _ from "lodash";
-interface EntityDefaultAttributes {
+interface EntityDefaultAttributes extends Hooks {
     type: string;
     owner: User | null;
     server: Server | null;
-
-    _create: () => boolean;
-    _init: (initialState: KeyValue) => void;
-    _tick: () => void;
-    _read: () => void;
-    _update: () => void;
-    _delete: () => boolean;
-    _gone: () => void;
-
     constructor?: Function;
 }
 
@@ -41,6 +32,17 @@ export class Entity extends HasId implements EntityDefaultAttributes {
         "constructor",
         "resetId",
     ];
+
+    public static isDefaultAttribute(attributeName: string): boolean {
+        for (const defaultAttributeName of this.defaultAttributes) {
+            if (
+                (attributeName === defaultAttributeName)
+                || (attributeName[0] === "_" && attributeName.substring(1) === defaultAttributeName)
+                || (defaultAttributeName[0] === "_" && defaultAttributeName.substring(1) === attributeName)
+            ) return true;
+        }
+        return false;
+    }
 
     /**
      * Clones an entity
@@ -86,60 +88,17 @@ export class Entity extends HasId implements EntityDefaultAttributes {
         this._owner = owner;
     }
 
-    /**
-     * @SharedIO Hook Function
-     *
-     * Called before an user attempts to create this entity.
-     * The return value (true or false) will determine whether or not the user will be able to create a new instance of this entity.
-     */
     public _create(): boolean {
         return true;
     }
 
-    /**
-     * @SharedIO Hook Function
-     *
-     * Called right after this entity is created.
-     * Similarly to a class constructor function, you should use this hook to initialize the entity.
-     */
-    public _init(initialState: KeyValue) {}
-
-    /**
-     * @SharedIO Hook Function
-     *
-     * This function will be called every server tick.
-     */
-    public _tick() {}
-
-    /**
-     * @SharedIO Hook Function
-     *
-     * Called before an user reads this entity's properties.
-     */
-    public _read() {}
-
-    /**
-     * @SharedIO Hook Function
-     *
-     * Called after an user updates this entity's properties and before the changes are propagated.
-     */
-    public _update() {}
-
-    /**
-     * @SharedIO Hook Function
-     *
-     * Called before an user attempts to delete this entity.
-     * The return value (true or false) will determine whether or not the user will be able to delete this entity.
-     */
     public _delete(): boolean {
         return true;
     }
 
-    /**
-     * @SharedIO Hook Function
-     *
-     * Called right after this entity gets deleted.
-     * Useful for implementing cleanup code.
-     */
+    public _init(initialState: KeyValue) {}
+    public _tick() {}
+    public _read() {}
+    public _update() {}
     public _gone() {}
 }
